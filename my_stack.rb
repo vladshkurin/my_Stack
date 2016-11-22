01861b3284e1d272d93ed8e7ecc21aaa66c67198
vladshkurin <shkurinvlad@gmail.com>
2016-11-22 17:08:09 +0200

    add benchmark tests

--- my_stack.rb
+++ my_stack.rb
@@ -1,7 +1,10 @@
+require 'benchmark'
+require 'pry'
+
 class MyStack
 
   def initialize
-   @stack = []
+    @stack = []
   end
 
   def push(value)
@@ -11,5 +14,53 @@ class MyStack
   def pop
     @stack.last
     @stack.delete_at(-1)
+  end
+
+  my_stack = MyStack.new
+
+  arr = Array.new
+
+  Benchmark.benchmark do |x|
+    x.report("1 million") do
+      1_000_000.times do
+        my_stack.push("RoR")
+      end
+      10_000_000.times do
+        my_stack.pop
+      end
+    end
+  end
+
+  Benchmark.benchmark do |x|
+    x.report("1 million index") do
+      1_000_000.times do |index|
+        arr[index] = "RoR"
+      end
+      1_000_000.times do
+        my_stack.pop
+      end
+    end
+  end
+
+  Benchmark.benchmark do |x|
+    x.report("10 million") do
+      10_000_000.times do
+        my_stack.push("RoR")
+      end
+      10_000_000.times do
+        my_stack.pop
+      end
+    end
+  end
+
+  Benchmark.benchmark do |x|
+    x.report("10 million index") do
+      10_000_000.times do |index|
+        arr[index] = "RoR"
+      end
+      10_000_000.times do
+        my_stack.pop
+      end
+    end
   end
 end
